fb2d6bc49af3de0f860850dc678370ea5033a273
maream khaled ahmed <mareamkmousbah@gmail.com>
2025-09-12 07:59:35 +0300

    Add Assets class for managing image and vector asset paths

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,22 @@
+class Assets {
+  Assets._();
+
+  static const String assetsImagesForgetPassword =
+      "assets/images/forget_password.png";
+
+  static const String assetsImagesOnboardingOne =
+      "assets/images/onboarding_one.png";
+
+  static const String assetsImagesOnboardingThree =
+      "assets/images/onboarding_three.png";
+
+  static const String assetsImagesOnboardingTwo =
+      "assets/images/onboarding_two.png";
+
+  static const String assetsImagesOrderSuccess =
+      "assets/images/order_success.png";
+
+  static const String assetsImagesVector1 = "assets/images/Vector1.svg";
+
+  static const String assetsImagesVector2 = "assets/images/Vector2.svg";
+}
