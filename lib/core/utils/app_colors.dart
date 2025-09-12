55590dc54852dd8d7cd94cd60d9e88c30cf30b98
maream khaled ahmed <mareamkmousbah@gmail.com>
2025-09-12 08:06:44 +0300

    Add AppColors class for centralized color management

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+abstract class AppColors {
+  static Color primaryColor = const Color(0xffC49E85);
+  static Color offWhite = const Color(0xffF8F4F9);
+  static Color black = const Color(0xff000000);
+  static Color white = const Color(0xffFFFFFF);
+  static Color deebGrey = const Color(0xff6F6460);
+  static Color lightGrey = const Color(0xffB4B4B4);
+  static Color grey = const Color(0xffC4C4C4);
+}
